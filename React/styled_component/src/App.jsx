69c5bfeda2fa28d094205dbd74268b5b55e9af9a
Irfan0001 <irfan93250@gmail.com>
2026-05-21 13:02:35 +0530

    first commit

---
+++ React/styled_component/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import Styled_component from './component/styled_component'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Styled_component/>
+    </>
+  )
+}
+
+export default App
